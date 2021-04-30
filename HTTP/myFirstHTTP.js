4b41c88bb2a23bca1f0786fd8d8bb5016ec90e75
mPrati-E3 <mattia.prati@energee3.com>
2021-04-30 16:58:29 +0200

    HTTP done

--- HTTP/myFirstHTTP.js
+++ HTTP/myFirstHTTP.js
@@ -6,20 +6,34 @@ function backgroundReadFile(url, callback) {
         callback(req.responseText);
     });
     req.send(null);
-  }
+}
   
-  function getURL(url, callback) {
+function getURL(url, callback) {
     var req = new XMLHttpRequest();
     req.open("GET", url, true);
     req.addEventListener("load", function() {
       if (req.status < 400)
         callback(req.responseText);
       else
-        callback(null, new Error("Request failed: " +
-                                 req.statusText));
+        callback(null, new Error("Request failed: " + req.statusText));
     });
     req.addEventListener("error", function() {
       callback(null, new Error("Network error"));
     });
     req.send(null);
+}
+
+const url = "https://eloquentjavascript.net/author";
+const types = ["text/plain",
+               "text/html",
+               "application/json",
+               "application/rainbows+unicorns"];
+
+/*async function showTypes() {
+  for (let type of types) {
+    let resp = await fetch(url, {headers: {accept: type}});
+    console.log(`${type}: ${await resp.text()}\n`);
   }
+}
+
+showTypes();*/
